ddab27d0784c96d9e6769df63db995991fc751f2
Scar Face <peteradaaku@gmail.com>
2024-06-10 08:49:27 +0100

    advanced interface mechanics

--- main.go
+++ main.go
@@ -4,3 +4,27 @@ type Storage interface {
 	Get(id int) (any, error)
 	Put(id int, val any) error
 }
+
+type FooStorage struct {
+}
+
+func (s *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
+type Server struct {
+	store Storage
+}
+
+func main() {
+	s := &Server{
+		store: &FooStorage{},
+	}
+	s.store.Get(1)
+	s.store.Put(1, "foo")
+
+}
