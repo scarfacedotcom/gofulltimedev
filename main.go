4f506e01c80b8e658af100f4d8e63d178f46f21c
Scar Face <peteradaaku@gmail.com>
2024-06-11 05:10:08 +0100

    typed functions

--- main.go
+++ main.go
@@ -1,54 +1,20 @@
 package main
 
-type Putter interface {
-	Put(id int, val any) error
+import (
+	"fmt"
+	"strings"
+)
+
+type TransformFunc func(s string) string
+
+func Uppercase(s string) string {
+	return strings.ToUpper(s)
 }
 
-type Storage interface {
-	Putter
-	Get(id int) (any, error)
-}
-type simplePutter struct {
-}
-
-func (s *simplePutter) Put(id int, val any) error {
-	return nil
-}
-
-type FooStorage struct {
-}
-
-func (s *FooStorage) Get(id int) (any, error) {
-	return nil, nil
-}
-
-func (s *FooStorage) Put(id int, val any) error {
-	return nil
-}
-
-type BarStorage struct {
-}
-
-func (s *BarStorage) Get(id int) (any, error) {
-	return nil, nil
-}
-
-func (s *BarStorage) Put(id int, val any) error {
-	return nil
-}
-
-type Server struct {
-	store Storage
-}
-
-func updateValue(id int, val any, p Putter) error {
-	return p.Put(id, val)
+func transformString(s string, fn TransformFunc) string {
+	return fn(s)
 }
 
 func main() {
-	s := &Server{
-		store: &FooStorage{},
-	}
-	updateValue(1, "petre", s.store)
-
+	fmt.Println(transformString("scar-face", Uppercase))
 }
---
+++ main3.go
@@ -0,0 +1,54 @@
+package main
+
+type Putter interface {
+	Put(id int, val any) error
+}
+
+type Storage interface {
+	Putter
+	Get(id int) (any, error)
+}
+type simplePutter struct {
+}
+
+func (s *simplePutter) Put(id int, val any) error {
+	return nil
+}
+
+type FooStorage struct {
+}
+
+func (s *FooStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *FooStorage) Put(id int, val any) error {
+	return nil
+}
+
+type BarStorage struct {
+}
+
+func (s *BarStorage) Get(id int) (any, error) {
+	return nil, nil
+}
+
+func (s *BarStorage) Put(id int, val any) error {
+	return nil
+}
+
+type Server struct {
+	store Storage
+}
+
+func updateValue(id int, val any, p Putter) error {
+	return p.Put(id, val)
+}
+
+func main3() {
+	s := &Server{
+		store: &FooStorage{},
+	}
+	updateValue(1, "petre", s.store)
+
+}
